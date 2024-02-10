73712a61a35f551c512b8d591d699dd71a882ff3
Kelf54 <kelf100500@gmail.com>
2024-02-10 11:04:39 +0700

    Начало реализации ReaderFile (осталась передача результата обработчикам)

--- src/main/java/org/kelf54/model/ReaderFile.java
+++ src/main/java/org/kelf54/model/ReaderFile.java
@@ -1,8 +1,27 @@
 package org.kelf54.model;
 
+import java.io.*;
+
 public class ReaderFile {
     public ReaderFile(){}
-    public void readFile(String patch){
+    public void readFile(String patch) throws IOException {
+        File fileIn = new File(patch);
+        if (!fileIn.exists()) throw new FileNotFoundException();
 
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileIn)))) {
+            String line = reader.readLine();
+            while (line != null) {
+                if (line.matches("\\d+")) { // целые числа
+                    //listInteger.add(Integer.parseInt(line));
+                } else if (line.matches("[+-]*\\d+[.,]\\d+")) { // вещественные числа
+                    //listFloat.add(Float.parseFloat(line));
+                } else { // строка
+                    //listString.add(line);
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new IOException(e);
+        }
     }
 }
